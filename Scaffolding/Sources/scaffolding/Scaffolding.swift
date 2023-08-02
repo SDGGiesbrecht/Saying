0d16660641397d6180c9047b1f557352b1ef6179
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-08-02 15:38:26 -0600

    Test workflow.

--- Scaffolding/Sources/scaffolding/Scaffolding.swift
+++ Scaffolding/Sources/scaffolding/Scaffolding.swift
@@ -3,6 +3,7 @@ import Foundation
 @main struct Scaffolding {
   static func main() throws {
     let thisFile = URL(fileURLWithPath: #filePath)
+    try FileManager.default.removeItem(at: thisFile)
     let packageRoot = thisFile
       .deletingLastPathComponent()
       .deletingLastPathComponent()
