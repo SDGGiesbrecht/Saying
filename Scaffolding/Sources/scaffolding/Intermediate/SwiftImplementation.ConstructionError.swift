69dacd459f6a7fa86c7fa1744857387ae576ba3a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-08-18 18:42:14 -0600

    Added parentheses warning.

--- Scaffolding/Sources/scaffolding/Intermediate/SwiftImplementation.ConstructionError.swift
+++ Scaffolding/Sources/scaffolding/Intermediate/SwiftImplementation.ConstructionError.swift
@@ -1,6 +1,7 @@
 extension SwiftImplementation {
   enum ConstructionError: Error {
     case literalError(LiteralIntermediate.ConstructionError)
+    case parenthesesMissing([ParsedImplementationComponent])
     case parameterNotFound(ParsedUninterruptedIdentifier)
   }
 }
