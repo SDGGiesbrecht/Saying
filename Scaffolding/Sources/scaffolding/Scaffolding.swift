64549afb5233e58439723fa7cbdaf61164e15b81
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-09-11 20:17:24 -0600

    Moved platform notes.

--- Scaffolding/Sources/scaffolding/Scaffolding.swift
+++ Scaffolding/Sources/scaffolding/Scaffolding.swift
@@ -10,6 +10,35 @@ import Foundation
       .deletingLastPathComponent()
     let package = Package(location: packageRoot)
 
+    // Platforms:
+
+    // macOS: Xcode (Swift Package Manager), Swift
+    // (1976‐04‐11 Apple Computer)
+
+    // Windows: Visual Studio (MSBuild), Windows App SDK, C#
+    // (1981‐08‐12 MS‐DOS)
+
+    // Web: HTML, JavaScript
+    // (1991‐08‐06)
+
+    // Ubuntu: GNOME Builder (Meson), GNOME, C
+    // (2004‐10‐20)
+
+    // tvOS: from macOS, Xcode (Swift Package Manager), Swift
+    // (2007‐01‐09)
+
+    // iOS: from macOS, Xcode (Swift Package Manager), Swift
+    // (2007‐06‐29)
+
+    // Android: from Linux, Android Studio, Kotlin
+    // (2008‐09‐23)
+
+    // Amazon Linux: MATE, Autoconf + Automake, GNU Build System, C
+    // (2011‐09‐26)
+
+    // watchOS: from macOS, Xcode (Swift Package Manager), Swift
+    // (2015‐04‐24)
+
     let arguments = ProcessInfo.processInfo.arguments.dropFirst()
     switch arguments.first {
     case "format":
