2c5868ee38d136440971aaf5bbc716e93418b61f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-09-16 00:54:04 -0600

    Cleaned out.

--- Construction/Kotlin/app/build.gradle.kts
+++ Construction/Kotlin/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -9,17 +8,4 @@ android {
     defaultConfig {
         minSdk = 21
     }
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.31")
-    testImplementation(
-            "org.assertj:assertj-core:3.12.2",
-            "org.junit.jupiter:junit-jupiter-api:5.4.2"
-    )
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.2")
-}
-
-test {
-    useJUnitPlatform()
 }
