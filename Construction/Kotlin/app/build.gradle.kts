c6d21bfdbe6670aa38acd1aa4b6ef1b59666b212
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-09-16 03:08:35 -0600

    Tried applying junit.

--- Construction/Kotlin/app/build.gradle.kts
+++ Construction/Kotlin/app/build.gradle.kts
@@ -14,3 +14,12 @@ android {
         minSdk = 21
     }
 }
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
--- Construction/Kotlin/app/src/test/kotlin/WrappedTests.kt
+++ Construction/Kotlin/app/src/test/kotlin/WrappedTests.kt
@@ -1,3 +1,5 @@
+import org.junit.Test
+
 class WrappedTests {
     @Test fun testProject() {
         test()
