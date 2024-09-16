17c5c0cf7b0a66f2f748c39b31c43c9b92639f38
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-09-16 02:16:43 -0600

    Fixed syntax.

--- Construction/Kotlin/app/build.gradle.kts
+++ Construction/Kotlin/app/build.gradle.kts
@@ -12,6 +12,6 @@ android {
 }
 
 dependencies {
-    implementation 'androidx.core:core-ktx:1.3.2'
-    implementation "org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version"
+    implementation("androidx.core:core-ktx:1.3.2")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.10")
 }
