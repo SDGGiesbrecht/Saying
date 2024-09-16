cb2de34c7016803d14f2a6a2ec6c51bc185bc70d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-09-16 03:19:13 -0600

    Tried without.

--- Construction/Kotlin/build.gradle.kts
+++ Construction/Kotlin/build.gradle.kts
@@ -2,7 +2,3 @@ plugins {
     id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
 }
-
-tasks.named<Test>("test") {
-    useJUnitPlatform()
-}
