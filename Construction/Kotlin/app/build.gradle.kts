935fdb968628ef7fad6a6d999e8df3efed0596db
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-09-16 03:14:31 -0600

    Tried other file.

--- Construction/Kotlin/app/build.gradle.kts
+++ Construction/Kotlin/app/build.gradle.kts
@@ -19,7 +19,3 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
-
-tasks.named<Test>("test") {
-    useJUnitPlatform()
-}
--- Construction/Kotlin/build.gradle.kts
+++ Construction/Kotlin/build.gradle.kts
@@ -2,3 +2,7 @@ plugins {
     id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
 }
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
