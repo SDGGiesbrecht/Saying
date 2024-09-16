048b3d2f9d35fe8cbc076716e405db70add01ff9
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-09-16 00:51:18 -0600

    Tried other quotes.

--- Construction/Kotlin/app/build.gradle.kts
+++ Construction/Kotlin/app/build.gradle.kts
@@ -12,12 +12,12 @@ android {
 }
 
 dependencies {
-    implementation('org.jetbrains.kotlin:kotlin-stdlib:1.3.31')
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.31")
     testImplementation(
-        'org.assertj:assertj-core:3.12.2',
-        'org.junit.jupiter:junit-jupiter-api:5.4.2'
+            "org.assertj:assertj-core:3.12.2",
+            "org.junit.jupiter:junit-jupiter-api:5.4.2"
     )
-    testRuntime('org.junit.jupiter:junit-jupiter-engine:5.4.2')
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.2")
 }
 
 test {
