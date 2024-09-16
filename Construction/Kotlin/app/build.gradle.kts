1f49c064726cc042cfac9a979447d2a4af55a5ed
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-09-16 00:47:51 -0600

    Tried specifying more test stuff.

--- Construction/Kotlin/app/build.gradle.kts
+++ Construction/Kotlin/app/build.gradle.kts
@@ -10,3 +10,16 @@ android {
         minSdk = 21
     }
 }
+
+dependencies {
+    implementation('org.jetbrains.kotlin:kotlin-stdlib:1.3.31')
+    testImplementation(
+        'org.assertj:assertj-core:3.12.2',
+        'org.junit.jupiter:junit-jupiter-api:5.4.2'
+    )
+    testRuntime('org.junit.jupiter:junit-jupiter-engine:5.4.2')
+}
+
+test {
+    useJUnitPlatform()
+}
