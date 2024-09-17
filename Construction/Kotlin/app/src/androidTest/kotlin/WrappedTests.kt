4924ad512f4d84e86bc54d5948a21a06bd3a0677
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-09-16 23:26:04 -0600

    Tried simplifying.

--- Construction/Kotlin/app/src/androidTest/kotlin/WrappedTests.kt
+++ Construction/Kotlin/app/src/androidTest/kotlin/WrappedTests.kt
@@ -1,11 +1,4 @@
-import org.junit.jupiter.api.Assertions.assertEquals
-
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
-
-This should not compile.
+import org.junit.Test
 
 class WrappedTests {
     @Test fun testProject() {
