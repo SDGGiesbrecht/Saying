44372fba7c912425b5ae45ca3fa7fc8923c437fd
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-09-17 00:31:33 -0600

    Removed presumably unneeded stuff.

--- Construction/Kotlin/app/build.gradle.kts
+++ Construction/Kotlin/app/build.gradle.kts
@@ -1,9 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    //id("org.jetbrains.kotlin.jvm")
 }
 
 kotlin {
@@ -17,21 +14,9 @@ android {
         minSdk = 21
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-    testOptions {
-        unitTests.all {
-            //useJUnitPlatform()
-        }
-    }
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.11.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     androidTestImplementation("androidx.test:runner:1.6.1")
     androidTestImplementation("androidx.test:rules:1.6.1")
 }
-
-//tasks.named<Test>("test") {
-//    useJUnitPlatform()
-//}
