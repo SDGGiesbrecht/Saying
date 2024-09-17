72172d971a983f507671b11606f2b11c4e342b20
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-09-17 00:32:02 -0600

    Tried without runner.

--- Construction/Kotlin/app/build.gradle.kts
+++ Construction/Kotlin/app/build.gradle.kts
@@ -17,6 +17,6 @@ android {
 }
 
 dependencies {
-    androidTestImplementation("androidx.test:runner:1.6.1")
+    //androidTestImplementation("androidx.test:runner:1.6.1")
     androidTestImplementation("androidx.test:rules:1.6.1")
 }
