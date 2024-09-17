f878ee8ab5440be95f7fb25974a9f7f9154fb302
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-09-17 00:21:40 -0600

    Tried changing syntax.

--- Construction/Kotlin/app/build.gradle.kts
+++ Construction/Kotlin/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     compileSdk = 34
     defaultConfig {
         minSdk = 21
-        testInstrumentationRunner("androidx.test.runner.AndroidJUnitRunner")
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     testOptions {
         unitTests.all {
