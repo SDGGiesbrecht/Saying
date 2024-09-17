dcf08d916fa34113fdb204fdd6abd2e216770789
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-09-16 22:42:10 -0600

    Compiling.

--- Construction/Kotlin/app/src/test/kotlin/WrappedTests.kt
+++ Construction/Kotlin/app/src/test/kotlin/WrappedTests.kt
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
-This should not compile.
-
 class WrappedTests {
     @Test fun testProject() {
         test()
