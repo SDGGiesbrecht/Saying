05d4b7d31cf4643c7298123a89a9b056fd82f5bd
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-09-16 22:21:25 -0600

    ...

--- Construction/Kotlin/build.gradle.kts
+++ Construction/Kotlin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.6.0" apply false
+    id("com.android.library") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-    id("org.jetbrains.kotlin.library") version "2.0.20" apply false
     //id("org.jetbrains.kotlin.jvm") version "2.0.20" apply false
 }
