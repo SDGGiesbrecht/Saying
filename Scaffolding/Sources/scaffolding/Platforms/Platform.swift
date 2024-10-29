94cbbf5ab98ce307f352008649ec625c52fa55f7
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-10-29 17:44:04 -0600

    Fixed native compliation error.

--- Scaffolding/Sources/scaffolding/Platforms/Platform.swift
+++ Scaffolding/Sources/scaffolding/Platforms/Platform.swift
@@ -167,8 +167,8 @@ extension Platform {
         return sanitize(identifier: type.names.identifier(), leading: true)
       }
     case .action(parameters: let parameters, returnValue: let returnValue):
-      #warning("Not implemented yet.")
-      return "???"
+      #warning("Hard coded.")
+      return "(Bool) -> Bool"
     }
   }
 
