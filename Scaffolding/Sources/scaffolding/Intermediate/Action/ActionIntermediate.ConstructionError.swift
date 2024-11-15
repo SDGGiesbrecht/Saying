9dd2c8350bb2eb43c9553c078d6fd2bc5fc041bb
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-11-14 20:17:28 -0600

    Removed dead code.

--- Scaffolding/Sources/scaffolding/Intermediate/Action/ActionIntermediate.ConstructionError.swift
+++ Scaffolding/Sources/scaffolding/Intermediate/Action/ActionIntermediate.ConstructionError.swift
@@ -5,8 +5,6 @@ extension ActionIntermediate {
     case brokenPrototype(ActionPrototype.ConstructionError)
     case unknownLanguage(ParsedUninterruptedIdentifier)
     case brokenNativeActionImplementation(NativeActionImplementationIntermediate.ConstructionError)
-    #warning("Is this dead?")
-    case parameterNotFound(ParsedUninterruptedIdentifier)
     case invalidImport(ParsedNativeActionImplementation)
     case missingImplementation(language: StrictString, action: ParsedActionName)
 
@@ -22,8 +20,6 @@ extension ActionIntermediate {
         return defaultMessage
       case .missingImplementation(let language, _):
         return "\(defaultMessage) (\(language))"
-      case .parameterNotFound:
-        return defaultMessage
       }
     }
 
@@ -39,8 +35,6 @@ extension ActionIntermediate {
         return implementation.location
       case .missingImplementation(_, action: let action):
         return action.location
-      case .parameterNotFound(let identifier):
-        return identifier.location
       }
     }
   }
