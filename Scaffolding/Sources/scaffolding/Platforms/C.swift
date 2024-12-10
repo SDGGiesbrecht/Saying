2ea00aeb229077972856577c87f8a97783dafba3
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-12-09 23:41:34 -0600

    Noted remaining problems.

--- Scaffolding/Sources/scaffolding/Platforms/C.swift
+++ Scaffolding/Sources/scaffolding/Platforms/C.swift
@@ -4,7 +4,7 @@ import SDGLogic
 import SDGCollections
 import SDGText
 
-#warning("exists, does not exist and unwrap if all rely of placeholder native implementations.")
+#error("exists, does not exist and unwrap if all rely of placeholder native implementations.")
 enum C: Platform {
 
   static var directoryName: String {
