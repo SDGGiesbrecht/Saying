06cb30974da83e172ae3a8c64ba7762c8e114c3b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-12-12 18:03:04 -0600

    Added package to help catalogue files.

---
+++ Source/Package.swift
@@ -0,0 +1,8 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+// This package does nothing. IDEs catalogue the source files in this directory better with it present.
+let package = Package(
+  name: "Saying"
+)
