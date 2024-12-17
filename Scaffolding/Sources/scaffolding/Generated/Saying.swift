e334edd467ec27e4e5d14a50698086531eace215
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2024-12-16 23:58:03 -0600

    Removed unnecessary parentheses.

--- Scaffolding/Sources/scaffolding/Generated/Saying.swift
+++ Scaffolding/Sources/scaffolding/Generated/Saying.swift
@@ -2,7 +2,7 @@ func compute(_ compute: () -> Bool, cachingIn cache: inout Bool?) -> Bool {
   if let cached = cache {
     return cached
   }
-  let result: Bool = (compute())
+  let result: Bool = compute()
   cache = ((result) as Bool?)
   return result
 }
