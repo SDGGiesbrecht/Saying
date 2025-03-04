a8976f152582fc1f8e46aaa1eb0609098358a944
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2025-03-03 20:32:34 -0600

    Fixed C?

--- Scaffolding/Sources/scaffolding/Intermediate/Action/ActionIntermediate.swift
+++ Scaffolding/Sources/scaffolding/Intermediate/Action/ActionIntermediate.swift
@@ -190,7 +190,7 @@ extension ActionIntermediate {
         swiftName: nil
       ),
       c: c ?? NativeActionImplementationIntermediate(
-        textComponents: ["", ".", ""].map({ UnicodeText($0) }),
+        textComponents: ["(", ").", ""].map({ UnicodeText($0) }),
         parameters: [
           NativeActionImplementationParameter(ParsedUninterruptedIdentifier(source: UnicodeText("container"))!),
           NativeActionImplementationParameter(ParsedUninterruptedIdentifier(source: UnicodeText("part"))!),
