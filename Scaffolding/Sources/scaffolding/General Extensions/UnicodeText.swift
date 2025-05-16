d4f26af9abe9d0f3437cc0779429290cef33f61b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2025-05-15 20:30:59 -0600

    Fixed build.

--- Scaffolding/Sources/scaffolding/General Extensions/UnicodeText.swift
+++ Scaffolding/Sources/scaffolding/General Extensions/UnicodeText.swift
@@ -1,1 +1,13 @@
+import SDGText
+
+extension UnicodeText {
+  init(_ string: String) {
+    self.init(string.unicodeScalars)
+  }
+
+  init(_ string: StrictString) {
+    self.init(String(string))
+  }
+}
+
 extension UnicodeText: Collection {}
