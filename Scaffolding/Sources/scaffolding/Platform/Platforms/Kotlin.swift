923f7902cdcaa4cc1c970b10388cbb6093d2da11
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2025-10-21 17:29:20 -0600

    Clean Up Skipping Actions (#197)
    Cleaned up skipping actions.

--- Scaffolding/Sources/scaffolding/Platform/Platforms/Kotlin.swift
+++ Scaffolding/Sources/scaffolding/Platform/Platforms/Kotlin.swift
@@ -408,6 +408,7 @@ enum Kotlin: Platform {
   static func createOtherProjectContainerFiles(projectDirectory: URL, dependencies: [String]) throws {
     try ([
       "android.useAndroidX=true",
+      "kotlin.daemon.jvmargs=-Xmx1g",
     ] as [String]).joined(separator: "\n").appending("\n")
       .save(to: projectDirectory.appendingPathComponent("gradle.properties"))
     try ([
