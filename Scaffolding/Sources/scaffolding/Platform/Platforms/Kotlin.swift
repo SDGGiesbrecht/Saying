12e8738b04f6b2db193917c1663ae7fc0e0ea802
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2025-11-16 21:18:11 -0600

    Increased JVM memory allowance.

--- Scaffolding/Sources/scaffolding/Platform/Platforms/Kotlin.swift
+++ Scaffolding/Sources/scaffolding/Platform/Platforms/Kotlin.swift
@@ -494,6 +494,7 @@ enum Kotlin: Platform {
   static func createOtherProjectContainerFiles(projectDirectory: URL, dependencies: [String]) throws {
     try ([
       "android.useAndroidX=true",
+      "org.gradle.jvmargs=-Xmx1g", // 512m ran out in the Android CI.
       "kotlin.daemon.jvmargs=-Xmx2g", // 1G ran out in the Android CI.
     ] as [String]).joined(separator: "\n").appending("\n")
       .save(to: projectDirectory.appendingPathComponent("gradle.properties"))
