a050a35113aa121ddf4579823f48c24ddea89027
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2025-12-03 23:50:12 -0600

    Removed recursion from format.

--- Scaffolding/Sources/scaffolding/Syntax/Node/SyntaxNode.swift
+++ Scaffolding/Sources/scaffolding/Syntax/Node/SyntaxNode.swift
@@ -16,35 +16,45 @@ extension SyntaxNode {
     return formattedGitStyleSource(indent: 0)
   }
   private func formattedGitStyleSource(indent: Int) -> UnicodeText {
-    switch nodeKind {
-    case .paragraphBreakSyntax:
-      return UnicodeText("\n\n" + String(repeating: " ", count: indent))
-    case .lineBreakSyntax:
-      return UnicodeText("\n" + String(repeating: " ", count: indent))
-    case .abilityDeclaration, .actionDeclaration, .caseDeclaration, .choiceDeclaration, .enumerationDeclaration, .extensionSyntax, .languageDeclaration, .nativeImport, .nativeIndirectRequirements, .nativeRequiredCode, .parameterDocumentation, .partDeclaration, .requirementDeclaration, .thingDeclaration, .use:
-      return UnicodeText(children.lazy.map({ $0.formattedGitStyleSource(indent: indent + 1) }).joined())
-    case .spacedNativeRequirementList:
-      return UnicodeText(
-        [
-          children.dropLast(1).map({ $0.formattedGitStyleSource(indent: indent + 1) }).joined(),
-          children.suffix(1).map({ $0.formattedGitStyleSource(indent: indent) }).joined(),
-        ].joined()
-      )
-    case .abilityName, .caseName, .cases, .documentation, .fulfillments, .multipleActionNames, .nonEmptyBracedStatementList, .parameterDetails, .paragraph, .partName, .provisions, .requirements, .sourceThingImplementation, .thingName:
-      return UnicodeText(
-        [
-          children.dropLast(2).map({ $0.formattedGitStyleSource(indent: indent + 1) }).joined(),
-          children.suffix(2).map({ $0.formattedGitStyleSource(indent: indent) }).joined(),
-        ].joined()
-      )
-    default:
-      if self is SyntaxLeaf {
-        return source()
-      } else {
-        return UnicodeText(
-          children.lazy.map({ $0.formattedGitStyleSource(indent: indent) }).joined()
+    var stack: [SyntaxNode] = [self]
+    var accumulator: String = ""
+    while !stack.isEmpty {
+      let node = stack.removeLast()
+      switch node.nodeKind {
+      case .paragraphBreakSyntax:
+        accumulator.append(contentsOf: "\n\n")
+        accumulator.append(contentsOf: String(repeating: " ", count: indent))
+      case .lineBreakSyntax:
+        accumulator.append("\n")
+        accumulator.append(contentsOf: String(repeating: " ", count: indent))
+      case .abilityDeclaration, .actionDeclaration, .caseDeclaration, .choiceDeclaration, .enumerationDeclaration, .extensionSyntax, .languageDeclaration, .nativeImport, .nativeIndirectRequirements, .nativeRequiredCode, .parameterDocumentation, .partDeclaration, .requirementDeclaration, .thingDeclaration, .use:
+        accumulator.append(
+          contentsOf: node.children
+            .lazy.map({ String($0.formattedGitStyleSource(indent: indent + 1)) })
+            .joined()
         )
+      case .spacedNativeRequirementList:
+        accumulator.append(
+          contentsOf: node.children.dropLast(1)
+            .map({ String($0.formattedGitStyleSource(indent: indent + 1)) })
+            .joined()
+        )
+        stack.append(contentsOf: node.children.suffix(1).reversed())
+      case .abilityName, .caseName, .cases, .documentation, .fulfillments, .multipleActionNames, .nonEmptyBracedStatementList, .parameterDetails, .paragraph, .partName, .provisions, .requirements, .sourceThingImplementation, .thingName:
+        accumulator.append(
+          contentsOf: node.children.dropLast(2)
+            .map({ String($0.formattedGitStyleSource(indent: indent + 1)) })
+            .joined()
+        )
+        stack.append(contentsOf: node.children.suffix(2).reversed())
+      default:
+        if node is SyntaxLeaf {
+          accumulator.append(contentsOf: String(node.source()))
+        } else {
+          stack.append(contentsOf: node.children.reversed())
+        }
       }
     }
+    return UnicodeText(accumulator)
   }
 }
