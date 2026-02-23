3129ce91685a2276fa4bfa507efff425c291c591
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2026-02-22 22:08:14 -0600

    Organized additive arithmetic.

--- Scaffolding/Sources/scaffolding/Generated/Saying.swift
+++ Scaffolding/Sources/scaffolding/Generated/Saying.swift
@@ -647,6 +647,12 @@ extension String.UnicodeScalarView {
 }
 
 extension UInt64 {
+  static var arithmeticZero: UInt64 {
+    return 0
+  }
+}
+
+extension UInt64 {
   static var one: UInt64 {
     return 1
   }
@@ -713,7 +719,7 @@ fileprivate func parse_0020line_0020in_0020_0028_0029_0020from_0020_0028_0029_00
 fileprivate func shim_0020unit_0020access_0020to_0020Git_2010style_0020line_0020parsing_003A() {
   let source_0020text: UnicodeText = UnicodeText(skippingNormalizationOf: " ...".unicodeScalars)
   let source: GitStyleSayingSource = GitStyleSayingSource(origin: UnicodeText(skippingNormalizationOf: "".unicodeScalars), code: source_0020text)
-  var beginning: Git_2010style_0020parsing_0020cursor? = Git_2010style_0020parsing_0020cursor(source_0020text.startIndex, UInt64.zero) as Git_2010style_0020parsing_0020cursor?
+  var beginning: Git_2010style_0020parsing_0020cursor? = Git_2010style_0020parsing_0020cursor(source_0020text.startIndex, .arithmeticZero) as Git_2010style_0020parsing_0020cursor?
   let end: Git_2010style_0020parsing_0020cursor = Git_2010style_0020parsing_0020cursor(source_0020text.endIndex, .one)
   var segments: [Unicode_0020segment] = []
   parse_0020line_0020in_0020_0028_0029_0020from_0020_0028_0029_0020to_0020_0028_0029_0020into_0020_0028_0029_003AGitStyleSayingSource_003A_0028_003Aoptional_0020_0028_0029_003AGit_2010style_0020parsing_0020cursor_003A_0029_003AGit_2010style_0020parsing_0020cursor_003A_0028_003Alist_0020of_0020_0028_0029_003AUnicode_0020segment_003A_0029_003A(source, &beginning, end, &segments)
