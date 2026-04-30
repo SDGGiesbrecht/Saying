b14b9fc54ca2eb96d48fa63c62716fab401d16af
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2026-04-30 15:54:51 -0600

    Restored positioning for Swift.

--- Scaffolding/Sources/scaffolding/Platform/Platform.swift
+++ Scaffolding/Sources/scaffolding/Platform/Platform.swift
@@ -2674,11 +2674,13 @@ extension Platform {
     var nativeRequirements: UniqueDeclaration?
     var nativeCondition: String?
     if let native = nativeImplementation(of: action) {
-      nativeRequirements = source(
-        for: native.requiredDeclarations,
-        referenceLookup: externalReferenceLookup,
-        alreadyHandled: &alreadyHandledNativeRequirements
-      ).map { UniqueDeclaration(full: $0, uniquenessDefinition: $0) }
+      if !isAbsorbedMember || self == CSharp.self {
+        nativeRequirements = source(
+          for: native.requiredDeclarations,
+          referenceLookup: externalReferenceLookup,
+          alreadyHandled: &alreadyHandledNativeRequirements
+        ).map { UniqueDeclaration(full: $0, uniquenessDefinition: $0) }
+      }
       if let condition = native.condition {
         nativeCondition = condition
       } else {
