66edc6c0aad1688639f524accedde621541d48fb
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2026-06-08 22:20:07 -0600

    Added cache.

--- Scaffolding/Sources/scaffolding/General Extensions/String.swift
+++ Scaffolding/Sources/scaffolding/General Extensions/String.swift
@@ -4,3 +4,12 @@ extension String {
     self.init(String.UnicodeScalarView(text))
   }
 }
+
+func compute(_ compute: () -> String, cachingIn cache: inout String?) -> String {
+  if let cached = cache {
+    return cached
+  }
+  let result: String = compute()
+  cache = result
+  return result
+}
